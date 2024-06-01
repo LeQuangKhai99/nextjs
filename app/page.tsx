25797651fc02ebc63b858004383976f35f6199fe
khai lee <fcxomben99@gmail.com>
2024-06-01 18:14:01 +0700

    add form

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
 import Image from "next/image";
 
 export default function Home() {
+  async function create(formData: FormData) {
+    'use server'
+    console.log('form data', formData.get('username'));
+    
+  }
+
   return (
-    <>Home page</>
+    <>
+      <div>Home page</div>
+      <div>
+        <form action={create}>
+          <input type="text" name="username" />
+          <button type="submit">submit</button>
+        </form>
+      </div>
+    </>
   );
 }
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,33 @@
+import { revalidateTag } from "next/cache";
 import UserTable from "../components/users/user.table";
 
 const UsersPage = async (props: any) => {
   const LIMIT = 1;
   const page = props?.searchParams?.page ?? 1;
 
+  async function create(formData: FormData) {
+    'use server'
+    console.log('form data', formData.get('username'));
+    
+    const rawResponse = await fetch('http://localhost:8000/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: formData.get('username'),
+        email: formData.get('email'),
+      })
+    });
+    const content = await rawResponse.json();
+    revalidateTag('list-user');
+    return content;
+  }
+
   const res: any = await fetch(`http://localhost:8000/users?page=${page}&limit=${LIMIT}`, {
     method: 'GET',
+    next: {tags: ['list-user']}
   });
   
   let data = await res.json();
@@ -14,6 +36,11 @@ const UsersPage = async (props: any) => {
 
   return (
     <div>
+      <form action={create}>
+        <input type="text" name="username" />
+        <input type="email" name="email" />
+        <button type="submit">submit</button>
+      </form>
       <UserTable 
         users={data || []}
         limit={LIMIT}
